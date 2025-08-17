b0ac744184923b2d56e6bf65cbca087122787a2c
Kostas Andreadis <kandread@umass.edu>
2025-08-17 06:25:09 -0400

    Fix negative size array

--- config.def.h
+++ config.def.h
@@ -38,8 +38,8 @@ static const char *const autostart[] = {
 static const Rule rules[] = {
         /* app_id             title       tags mask     isfloating   monitor */
         /* examples: */
-        /* { "Gimp_EXAMPLE",     NULL,       0,            1,           -1 }, /\* Start on currently visible tags floating, not tiled *\/ */
-        /* { "firefox_EXAMPLE",  NULL,       1 << 8,       0,           -1 }, /\* Start on ONLY tag "9" *\/ */
+        { "Gimp_EXAMPLE",     NULL,       0,            1,           -1 }, /* Start on currently visible tags floating, not tiled */
+        { "firefox_EXAMPLE",  NULL,       1 << 8,       0,           -1 }, /* Start on ONLY tag "9" */
 };
 
 /* layout(s) */
